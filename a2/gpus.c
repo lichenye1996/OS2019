8073668802ae524c6b593e20de507bb1df69809b
李晨埜 <lichenye@dhcp-clara-3364.eduroam.cornell.edu>
2019-10-04 03:41:31 -0400

    gpu problem done

--- a2/gpus.c
+++ a2/gpus.c
@@ -8,6 +8,13 @@ rthread_lock_t print_lock;
 #include <assert.h>
 struct gpu_info
 {
+    int allocated[NGPUS];
+    unsigned int nfree;
+    rthread_sema_t mutex;
+    rthread_sema_t allocateSema;
+
+    int waitRequests;
+    int nAllocate;
 };
 
 void gi_init(struct gpu_info *gi);
@@ -18,18 +25,68 @@ void gi_release(struct gpu_info *gi,
                 unsigned int ngpus,
                 /* IN */ unsigned int gpus[]);
 void gi_free(struct gpu_info *gi);
+void gi_vacate(struct gpu_info *gi);
+
+
 
 void gi_init(struct gpu_info *gi) {
+    memset(gi->allocated, 0, sizeof(gi->allocated));
+    gi->nfree = NGPUS;
+    rthread_sema_init(&gi->mutex, 1);
+    rthread_sema_init(&gi->allocateSema, 0);
+    gi->waitRequests = 0;
+    gi->nAllocate = 0;
 }
 
-void gi_alloc(struct gpu_info *gi,
-              unsigned int ngus,
-	      unsigned int gpus[]) {
+void gi_vacate(struct gpu_info *gi){
+    if (gi->nAllocate == 0 && gi->waitRequests > 0) {
+        gi->waitRequests--;
+        rthread_sema_vacate(&gi->allocateSema);
+    }
+    else {
+        rthread_sema_vacate(&gi->mutex);
+    }
 }
 
-void gi_release(struct gpu_info *gi,
-                unsigned int ngpus,
-                /* IN */ unsigned int gpus[]) {
+void gi_alloc(struct gpu_info *gi, unsigned int ngpus, unsigned int gpus[]) {
+    rthread_sema_procure(&gi->mutex);
+    //assert(ngpus <= gi->nfree);
+    if (ngpus <= NGPUS) {
+        if (ngpus > gi->nfree) {
+            gi->waitRequests++;
+            gi_vacate(gi);
+            rthread_sema_procure(&gi->allocateSema);
+        }
+        
+        gi->nAllocate++;
+        gi->nfree -= ngpus;
+        unsigned int g = 0;
+        for (unsigned int i = 0; g < ngpus; i++) {
+            assert(i < NGPUS);
+            if (!gi->allocated[i]) {
+                gi->allocated[i] = 1;
+                gpus[g++] = i;
+            }
+        }
+        gi_vacate(gi);
+    }
+    else {
+        assert(ngpus <= NGPUS);
+    }
+    
+}
+
+void gi_release(struct gpu_info *gi, unsigned int ngpus, /* IN */ unsigned int gpus[]) {
+    rthread_sema_procure(&gi->mutex);
+    for (unsigned int g = 0; g < ngpus; g++) {
+        assert(gpus[g] < NGPUS);
+        assert(gi->allocated[gpus[g]]);
+        gi->allocated[gpus[g]] = 0;
+    }
+    gi->nfree += ngpus;
+    assert(gi->nfree <= NGPUS);
+    gi->nAllocate--;
+    gi_vacate(gi);
 }
 
 void gi_free(struct gpu_info *gi) {
@@ -50,6 +107,8 @@ void gpu_user(void *shared, void *arg)
         unsigned int n = 1 + (random() % NGPUS);
         rthread_with(&print_lock)
         printf("%s %d wants %u gpus\n", (char *) arg, i, n);
+        printf("****** in = %d, wait = %d\n",gi->nAllocate, gi->waitRequests);
+
         gi_alloc(gi, n, gpus);
         rthread_with(&print_lock)
         {
@@ -58,10 +117,12 @@ void gpu_user(void *shared, void *arg)
                 printf(" %d", gpus[i]);
             printf("\n");
         }
+        printf("****** in = %d, wait = %d\n",gi->nAllocate, gi->waitRequests);
         rthread_delay(random() % 3000);
         rthread_with(&print_lock)
             printf("%s %d releases gpus\n", (char *) arg, i);
         gi_release(gi, n, gpus);
+        printf("****** in = %d, wait = %d\n",gi->nAllocate, gi->waitRequests);
     }
 }
 int main()
